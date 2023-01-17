1fb16bcea382483e5ddfe9de4bdffb65bfe1fe06
Mamdouh93Murad <mamdouh93morad@gmail.com>
2023-01-17 02:10:37 +0200

    m

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,24 +1,24 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-# export POSTGRES_USERNAME=postgres
-# export POSTGRES_PASSWORD=postgres
-# export POSTGRES_HOST=database-1.cv7igqzhozlx.us-east-1.rds.amazonaws.com
-# export POSTGRES_DB=postgres
-# export AWS_BUCKET=arn:aws:s3:::awsmybucket-mamdouh
-# export AWS_REGION=us-east-1
-# export AWS_PROFILE=default
-# export JWT_SECRET=mysecretstring
-# export URL=http://localhost:8100
-
-export AWS_ACCESS_KEY_ID = AKIAZM3EKE3QQGO3ZTRW
-export AWS_BUCKET = arn:aws:s3:::awsmybucket-mamdouh
-export AWS_DEFAULT_REGION = us-east-1
-export AWS_PROFILE = default
-export AWS_REGION = us-east-1
-export AWS_SECRET_ACCESS_KEY = gw12lYqKtEk8lsuiw8j2D2CWTnppx2FCv7pv1+br
-export JWT_SECRET = mysecretstring
-export POSTGRES_DB = postgres
-export POSTGRES_HOST: =database-1.cv7igqzhozlx.us-east-1.rds.amazonaws.com
-export POSTGRES_PASSWORD = postgres
-export POSTGRES_USERNAME = postgres
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cv7igqzhozlx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::awsmybucket-mamdouh
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
+
+# export AWS_ACCESS_KEY_ID = AKIAZM3EKE3QQGO3ZTRW
+# export AWS_BUCKET = arn:aws:s3:::awsmybucket-mamdouh
+# export AWS_DEFAULT_REGION = us-east-1
+# export AWS_PROFILE = default
+# export AWS_REGION = us-east-1
+# export AWS_SECRET_ACCESS_KEY = gw12lYqKtEk8lsuiw8j2D2CWTnppx2FCv7pv1+br
+# export JWT_SECRET = mysecretstring
+# export POSTGRES_DB = postgres
+# export POSTGRES_HOST: =database-1.cv7igqzhozlx.us-east-1.rds.amazonaws.com
+# export POSTGRES_PASSWORD = postgres
+# export POSTGRES_USERNAME = postgres
+# export URL=http://localhost:8100
