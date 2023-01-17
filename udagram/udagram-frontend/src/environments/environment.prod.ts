b1beffb6771f55d0860f991efb629dce90432837
Mamdouh93Murad <mamdouh93morad@gmail.com>
2023-01-17 10:04:42 +0200

    send env

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-awimw342.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagramapi-env.eba-hvjxnumw.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
